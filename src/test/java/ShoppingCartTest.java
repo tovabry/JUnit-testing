52f6d99d5ec8b17e8e9f74b369be36a9f108d2c0
Tova <tova.bryngelsson@live.se>
2025-01-31 13:54:38 +0100

    ShoppingCart + test setup

---
+++ src/test/java/ShoppingCartTest.java
@@ -0,0 +1,12 @@
+import com.example.ShoppingCart;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ShoppingCartTest {
+    private ShoppingCart cart;
+
+    @BeforeEach
+    void setUp() {
+        cart = new ShoppingCart();
+    }
+
+}
